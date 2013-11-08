eaeb7bedc906056afff8ea07a3ab480bdf41d22b
Vido <vido@usp.br>
2013-11-08 18:33:58 -0200

    Issue #2. The Qemu VM is now ok. This are the early eforts to deploy the STOQ system on ARM arch.

---
+++ tools/configure_vm.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+
+# TODO
+# wget raw.github.com/Vido/floydberry/tools/setup.sh 
+
+
+
+
--- tools/fix_raspbian_image.sh
+++ tools/fix_raspbian_image.sh
@@ -9,13 +9,15 @@ if [[ $answer = y ]] ; then
     wget http://xecdesign.com/downloads/linux-qemu/kernel-qemu 
 fi
 
-#unzip '*.zip'
+
+unzip '*.zip'
+qemu-img *.img +2G
+
 sudo mount *.img -o offset=62914560 /mnt
 
-sudo echo "# /usr/lib/arm-linux-gnueabihf/libcofi_rpi.so" > /mnt/etc/ld.so.preload
+sudo echo "#/usr/lib/arm-linux-gnueabihf/libcofi_rpi.so" > /mnt/etc/ld.so.preload
 sudo echo "KERNEL==\"sda\", SYMLINK+=\"mmcblk0\"" > /mnt/etc/udev/rules.d/90-qemu.rules
 sudo echo "KERNEL==\"sda?\", SYMLINK+=\"mmcblk0p%n\"," >> /mnt/etc/udev/rules.d/90-qemu.rules
-
 sudo sync
 
 sudo umount /mnt
